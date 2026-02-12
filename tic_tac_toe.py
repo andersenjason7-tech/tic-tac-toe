f2212a7d7f614881e046b18e04e7dc3e78399a72
Jason Andersen <andersen.jason7@gmail.com>
2026-02-11 20:38:38 -0700

    adding more functionality

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,9 +1,93 @@
 """Module providing a function printing python version."""
 
 import tkinter as tk
+from itertools  import cycle
 from tkinter import font
+from typing import NamedTuple
 
 
+class Player(NamedTuple):
+    """_summary_ Holds the player info
+
+    Args:
+        NamedTuple (_type_): _description_
+    """
+    label: str
+    color: str 
+class Move(NamedTuple):
+    """_summary_ Handles the move
+
+    Args:
+        NamedTuple (_type_): _description_
+    """
+    row: int
+    col: int
+    label: str = ""
+BOARD_SIZE = 3
+DEFAULT_PLAYERS = (
+    Player(label="X", color="blue"),
+    Player(label="O", color="green"),
+)
+
+
+class TicTacToeGame:
+    def __init__(self, players=DEFAULT_PLAYERS, board_size=BOARD_SIZE):
+        self._players = cycle(players)
+        self.board_size = board_size
+        self.current_player = next(self._players)
+        self.winner_combo = []
+        self._current_moves = []
+        self._has_winner = False
+        self._winning_combos = []
+        self._setup_board()
+    def _setup_board(self):
+        """Set up the board"""
+        self._current_moves = [
+            [Move(row, col) for col in range(self.board_size)]
+            for row in range(self.board_size)
+        ]
+        self._winning_combos = self._get_winning_combos()
+    def _get_winning_combos(self):
+        """Get winning combos"""
+        rows = [
+            [(move.row, move.col) for move in row]
+            for row in self._current_moves
+        ]
+        columns = [list(col) for col in zip(*rows)]
+        first_diagonal = [row[i] for i, row in enumerate(rows)]
+        second_diagonal = [col[j] for j, col in enumerate(reversed(columns))]
+        return rows + columns + [first_diagonal, second_diagonal]
+    def is_valid_move(self, move):
+        """Return True if move is valid, and False otherwise."""
+        row, col = move.row, move.col
+        move_was_not_played = self._current_moves[row][col].label == ""
+        no_winner = not self._has_winner
+        return no_winner and move_was_not_played
+    def process_move(self, move):
+        """Process the current move and check if it's a win."""
+        row, col = move.row, move.col
+        self._current_moves[row][col] = move
+        for combo in self._winning_combos:
+            results = set(
+                self._current_moves[n][m].label
+                for n, m in combo
+            )
+            is_win = (len(results) == 1) and ("" not in results)
+            if is_win:
+                self._has_winner = True
+                self.winner_combo = combo
+                break
+    def has_winner(self):
+        """Return True if the game has a winner, and False otherwise."""
+        return self._has_winner
+    def is_tied(self):
+        """Return True if the game is tied, and False otherwise."""
+        no_winner = not self._has_winner
+        played_moves = (
+            move.label for row in self._current_moves for move in row
+        )
+        return no_winner and all(played_moves)
+    
 class TicTacToeBoard(tk.Tk):
     """_summary_
 
