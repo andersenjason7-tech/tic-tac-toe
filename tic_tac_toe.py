bac36526b9f92e57619d9380c5698c883db3bdeb
Jason Andersen <andersen.jason7@gmail.com>
2026-02-11 19:54:27 -0700

    more functionality

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,8 +1,60 @@
+"""Module providing a function printing python version."""
+
+import sys
 import tkinter as tk
 from tkinter import font
 
+
+def print_python_version():
+    """Function printing python version."""
+    print(sys.version)
+
+
+
 class TicTacToeBoard(tk.Tk):
+    """_summary_
+
+    Args:
+        tk (_type_): _description_
+    """
     def __init__(self):
         super().__init__()
         self.title("Tic-Tac-Toe Game")
         self._cells = {}
+        self._create_board_display()
+        self._create_board_grid()
+
+
+    def _create_board_display(self):
+        display_frame = tk.Frame(master=self)
+        display_frame.pack(fill=tk.X)
+        self.display = tk.Label(
+            master=display_frame,
+            text="Ready?",
+            font=font.Font(size=28, weight="bold"),
+        )
+        self.display.pack()
+    def _create_board_grid(self):
+        grid_frame = tk.Frame(master=self)
+        grid_frame.pack()
+        for row in range(3):
+            self.rowconfigure(row, weight=1, minsize=50)
+            self.columnconfigure(row, weight=1, minsize=75)
+            for col in range(3):
+                button = tk.Button(
+                    master=grid_frame,
+                    text="",
+                    font=font.Font(size=36, weight="bold"),
+                    fg="black",
+                    width=3,
+                    height=2,
+                    highlightbackground="lightblue",
+                )
+                self._cells[button] = (row, col)
+                button.grid(
+                    row=row,
+                    column=col,
+                    padx=5,
+                    pady=5,
+                    sticky="nsew"
+                )
